bde471dd2463f8e047a428ce5539afea65d62983
Tarun Lalwani <tarunlalwani2002@gmail.com>
2023-01-07 13:49:37 +0530

    Community component desktop and mobile view

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import CommunityComponent from '../components/community/community'
 import FooterComponent from '../components/footer/footer'
 import HeaderComponent from '../components/header/header'
 
 export default function Main() {
   return (
     <div>
+      <CommunityComponent />
       <HeaderComponent />
 
       <FooterComponent />
