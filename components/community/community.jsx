7550a5ff12e2bb20441c47c48ad524a95353ef19
Tarun Lalwani <tarunlalwani2002@gmail.com>
2023-01-07 15:03:51 +0530

    responsiveness corrected

--- components/community/community.jsx
+++ components/community/community.jsx
@@ -10,7 +10,7 @@ export default function CommunityComponent() {
             </div>
             <div className={styles.body}>
                 <div className={styles.bodyl}>
-                    <figure><img src="/assets/bootcamp.png" alt="" srcset="" /><figcaption>An enthusiastic community</figcaption></figure>
+                    <figure><img src="/assets/bootcamp.png" alt="" srcSet="" /><figcaption>An enthusiastic community</figcaption></figure>
                 </div>
                 <div className={styles.bodym}>
                     <div className={styles.line}></div>
@@ -18,8 +18,8 @@ export default function CommunityComponent() {
                     <div className={styles.line}></div>
                 </div>
                 <div className={styles.bodyr}>
-                    <figure><img src="/assets/bootcamp.png" alt="" srcset="" /><figcaption>An enthusiastic community</figcaption></figure>
-                    <figure><img src="/assets/bootcamp2.png" alt="" srcset="" /><figcaption>An enthusiastic community</figcaption></figure>
+                    <figure><img src="/assets/bootcamp.png" alt="" srcSet="" /><figcaption>An enthusiastic community</figcaption></figure>
+                    <figure><img src="/assets/bootcamp2.png" alt="" srcSet="" /><figcaption>An enthusiastic community</figcaption></figure>
                 </div>
             </div>
             <img className={styles.registerNow} src="/assets/headerRegister.png" alt="" />
